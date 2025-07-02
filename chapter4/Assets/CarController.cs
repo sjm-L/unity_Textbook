13589f7211f435ddd548660bb491fc153681108c
sjm-L <shower1300@gmail.com>
2025-07-02 11:04:02 +0900

    작동이 안되서 Debug.Log추가 Both변경후 작동

--- chapter4/Assets/CarController.cs
+++ chapter4/Assets/CarController.cs
@@ -14,23 +14,23 @@ public class CarController:MonoBehaviour
 
     void Update()
     {
-      //스와이프의 길이를 구한다.
-      if(Input.GetMouseButtonDown(0))
-      {
-        this.startPos = Input.mousePosition;
-      }
-      else if (Input.GetMouseButtonUp(0))
-      {
-        //마우스 버튼에서 손가락을 떼었을 때 좌포
-        Vector2 endPos = Input.mousePosition;
-        float swipeLength = endPos.x - this.startPos.x;
+    //스와이프의 길이를 구한다.
+    if (Input.GetMouseButtonDown(0))
+    {
+      this.startPos = Input.mousePosition;
+    }
+    else if (Input.GetMouseButtonUp(0))
+    {
+      //마우스 버튼에서 손가락을 떼었을 때 좌포
+      Vector2 endPos = Input.mousePosition;
+      float swipeLength = endPos.x - this.startPos.x;
 
-        //스와이프 길이를 처음 속도로 변환한다.
-        this.speed = swipeLength / 500.0f;
+      //스와이프 길이를 처음 속도로 변환한다.
+      this.speed = swipeLength / 500.0f;
 
-        //효과음을 재생한다.
-        GetComponent<AudioSource>().Play();
-      }
+      //효과음을 재생한다.
+      GetComponent<AudioSource>().Play();
+    }
       transform.Translate(this.speed, 0 ,0); //이동
       this.speed *= 0.98f;                   //감속
     }
--- chapter5/Assets/PlrayerController.cs
+++ chapter5/Assets/PlrayerController.cs
@@ -13,11 +13,13 @@ public class PlrayerController : MonoBehaviour
     //왼쪽 화살표가 눌렀을 때
     if (Input.GetKeyDown(KeyCode.LeftArrow))
     {
+      Debug.Log("왼쪽");
       transform.Translate(-3, 0, 0); // 왼쪽으로 '3' 움직인다.
     }
     //오른쪽 화살표가 눌렀을 때
     if (Input.GetKeyDown(KeyCode.RightArrow))
     {
+      Debug.Log("오른쪽");
       transform.Translate(3, 0, 0); //오른쪽으로 '3' 움직인다.
     }
   }
