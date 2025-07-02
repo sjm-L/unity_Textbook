2f5b84a7b6d7a054552c0b0fdf271f70efabddd3
sjm-L <shower1300@gmail.com>
2025-07-02 11:30:54 +0900

    충돌 판정추가

--- chapter5/Assets/ArrowController.cs
+++ chapter5/Assets/ArrowController.cs
@@ -3,9 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 public class ArrowController : MonoBehaviour
 {
+  GameObject player;
   void Start()
   {
-
+    this.player = GameObject.Find("player_0");
   }
 
   void Update()
@@ -17,6 +18,20 @@ public class ArrowController : MonoBehaviour
     if (transform.position.y < -5.0f)
     {
       Destroy(gameObject);
-      }
+    }
+
+    //출돌판정
+    Vector2 p1 = transform.position;              //화살의 중심 좌표
+    Vector2 p2 = this.player.transform.position;  //플레이어의 중심 좌표
+    Vector2 dir = p1 - p2;
+    float d = dir.magnitude;
+    float r1 = 0.5f;                              //화살의 반지름
+    float r2 = 1.0f;                              //플레이어의 반지름
+
+    if (d < r1 + r2)
+    {
+      //출돌한 경우는 화살을 지운다.
+      Destroy(gameObject);
+    }
   }
 }
